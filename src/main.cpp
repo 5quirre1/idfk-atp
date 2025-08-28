28c784ddb706042d0f62a7ba94385566932e495c
Squirrel <squirrelhomebrew@gmail.com>
2025-08-27 18:11:03 -0700

    f

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include "raylib.h"
 #include "game.h"
+#include "menu.h"
 
 int main() {
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "idfk atp"); // init the window
-    InitAudioDevice(); // for footstep sounds
+    InitWindow(screenWidth, screenHeight, "idfk"); // init the window
+    InitAudioDevice(); // for footstep sounds and menu sounds
     SetTargetFPS(60); // fps
+    SetExitKey(0); // don't make esc close window
 
     // icon
     Image icon = LoadImage("assets/dog.png");
@@ -15,10 +17,41 @@ int main() {
     SetWindowIcon(icon);
     UnloadImage(icon);
 
-    // run the game loop
-    RunGame();
+    // initialize menu
+    Menu menu = InitMenu();
+    MenuState gameState = MENU_STATE_MAIN;
+    bool inGame = false;
 
-    // cleanup
+    // main loop
+    while (!WindowShouldClose() && gameState != MENU_STATE_EXIT) {
+        if (!inGame) {
+            gameState = UpdateMenu(&menu);
+            
+            if (gameState == MENU_STATE_GAME) {
+                inGame = true;
+                EnableCursor();
+                DisableCursor();
+            }
+            
+            BeginDrawing();
+                ClearBackground(RAYWHITE);
+                DrawMenu(&menu);
+            EndDrawing();
+        } else {
+            if (IsKeyPressed(KEY_ESCAPE)) {
+                inGame = false;
+                gameState = MENU_STATE_MAIN;
+                EnableCursor();
+                continue;
+            }
+            RunGame();
+            inGame = false;
+            gameState = MENU_STATE_MAIN;
+            EnableCursor();
+        }
+    }
+
+    UnloadMenu(&menu);
     CloseAudioDevice();
     CloseWindow();
 
